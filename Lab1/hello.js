38af1474ee4ac91eac9c93fdcaa5ead250339c2e
Anand Dubey <anand876770@gmail.com>
2026-07-21 14:49:46 +0530

    add function

---
+++ Lab1/hello.js
@@ -0,0 +1,13 @@
+function sayHello(name){
+    console.log(`welcome to ${name}`);
+
+}
+
+sayHello(`Anand Dubey`)
+
+function sum(a,b){
+    return a+b;
+}
+
+console.log(`sum of a and b is ${sum(10,15)}`);
+
