b6f1a7aa1a5a58332a634b22b9407d9e15e3ecb6
Anand Dubey <anand876770@gmail.com>
2026-08-25 14:57:34 +0530

    prg2

---
+++ lab3/prg2.js
@@ -0,0 +1,6 @@
+import http from "http"
+const server=http.createServer((req,res)=>{
+    res.write("<h1>Hello Client</h1>");
+    res.end();
+});
+server.listen((4444,()=>console.log("server is running at 444...")));
