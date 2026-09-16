aef3c19c30f432b8cee441a7b64ff6f1f0d62f3e
Anand Dubey <anand876770@gmail.com>
2026-09-16 12:04:36 +0530

    add post request with body

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -4,8 +4,20 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.end("Get Request");
   } else if(req.url === "/"&& req.method ==="POST"){
-    res.statusCode = 200;
+    //console.log("request:",req);
+    let body=''
+    req.on('data',(chunk)=>{
+      body+=chunk
+    })
+    req.on("end",()=>{
+      const product=JSON.parse(body);
+      console.log("recieved product:",product);
+
+      
+    res.statusCode = 201;
     res.end("Post request");
+    });
+
   }
   else if(req.url === "/"&& req.method ==="PUT"){
     res.statusCode = 200;
