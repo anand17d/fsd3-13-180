46d3b89ba77d8b963b8cd96d5cff6166c655351d
Anand Dubey <anand876770@gmail.com>
2026-09-22 14:17:24 +0530

    get all products

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,43 +1,53 @@
 import http from "http";
+import { getAllProducts } from "./products.js";
 const server = http.createServer((req, res) => {
-  if (req.url === "/"&& req.method ==="GET") {
+ if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
+    const data = getAllProducts();
+    res.setHeader('content-type','application/json')
+    res.end(JSON.stringify({
+           count:data.length,
+           data,
+    })),
     res.end("Get Request");
-  } else if(req.url === "/"&& req.method ==="POST"){
-    //console.log("request:",req);
-    let body=''
-    req.on('data',(chunk)=>{
-      body+=chunk
-    })
-    req.on("end",()=>{
-      const product=JSON.parse(body);
-      console.log("recieved product:",product);
-
-      
-    res.statusCode = 201;
-    res.end("Post request");
-    });
-
   }
-  else if(req.url.startsWith("/products") === "/"&& req.method ==="PUT"){
-    const productID  = req.url.split('/').pop();
-    console.log("update product id:",productID);
+  else if (req.url === "/api/v1/products" && req.method === "POST") {
+    // console.log("Request:",req);
     let body = "";
-    req.on("data",(chunk)=>{
-         body+=chunk;
-
+    req.on("data", (chunk) => {
+      body += chunk;
     });
-
+    req.on("end", () => {
+      const product = JSON.parse(body);
+      console.log("received product:", product);
+      res.statusCode = 201;
+      res.end(JSON.stringify({ msg: "product added", product }));
+    });
+  } 
+  
+  else if (req.url.startsWith( "/products/") && req.method === "PUT") {
+    const productID = req.url.split('/').pop();
+    console.log('Update Product id:',productID);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+    req.on("end", () => {
+      const product = JSON.parse(body);
+      product.id = productID
+      res.statusCode = 200;
+      res.end(JSON.stringify({ msg: "product updated", product }));
+    });
+    
+  } 
+  
+  else if (req.url === "/" && req.method === "DELETE") {
     res.statusCode = 200;
-    res.end("Post request");
-  }
-  else if(req.url === "/"&& req.method ==="DELETE"){
-    res.statusCode = 200;
-    res.end("DELETE request");
-  }
-  else{
-    res.statusCode=200;
+    res.end("DELETE Request");
+  } else {
+    res.statusCode = 404;
     res.end("request not found");
   }
 });
+
 server.listen(6000, () => console.log("prg6 is running"));
---
+++ lab3/products.js
@@ -0,0 +1,13 @@
+const products = [
+  { id: 1, name: "marker", qty:100,price: 10 },
+  { id: 2, name: "Pen", qty:50, price: 20 },
+  { id: 3, name: "Pencil", qty:200, price: 50 },
+];
+let nextId = 3;
+
+export const getAllProducts = ()=>{
+    return products;
+}
+export const addProduct = ()=>{
+    
+}
