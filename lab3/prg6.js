461fe1b4c407007f13a23d4310052e70e3e7737e
Anand Dubey <anand876770@gmail.com>
2026-09-22 14:34:25 +0530

    post all products

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,5 +1,6 @@
 import http from "http";
-import { getAllProducts } from "./products.js";
+import { getAllProducts,addProduct } from "./products.js";
+
 const server = http.createServer((req, res) => {
  if (req.url === "/api/v1/products" && req.method === "GET") {
     res.statusCode = 200;
@@ -19,9 +20,11 @@ const server = http.createServer((req, res) => {
     });
     req.on("end", () => {
       const product = JSON.parse(body);
-      console.log("received product:", product);
+      const item = addProduct(product);
       res.statusCode = 201;
-      res.end(JSON.stringify({ msg: "product added", product }));
+      res.end(JSON.stringify({ msg: "product added", data: item
+
+       }));
     });
   } 
   
